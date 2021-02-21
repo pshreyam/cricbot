de8ea11e564aafce762b3bf3dc4f4e7ffcd18c83
Shreyam Pokharel <pshreyam@gmail.com>
2021-02-21 20:33:47 +0545

    bot working-0.0.1

--- bot.py
+++ bot.py
@@ -5,6 +5,8 @@ import discord
 from dotenv import load_dotenv
 load_dotenv()
 
+from provider import get_fixtures, get_recent_matches, get_score
+
 class BotClient(discord.Client):
     async def on_connect(self):
         print('Connecting to discord!')
@@ -23,7 +25,7 @@ class BotClient(discord.Client):
                         title='Help for CricHelperBot!',
                         description='CricHelperBot is intended to help you with \
                         cricket scores and fixtures!',
-                        color=discord.Color.blue(),
+                        color=discord.Color.blue()
                     )
                     help_embed.add_field(
                         name='?fixtures', 
@@ -31,19 +33,58 @@ class BotClient(discord.Client):
                         inline=True
                     )
                     help_embed.add_field(
-                        name='?scores', 
-                        value='get scores for currently active matches',
+                        name='?results', 
+                        value='get results for recent matches',
+                        inline=True
+                    )
+                    help_embed.add_field(
+                        name='?score <match_id>', 
+                        value='get score for the match with id',
                         inline=True
                     )
                     await message.reply(embed=help_embed)
                 elif cmd == 'fixtures':
-                    await message.reply(
-                        'Sorry! \'fixtures\' feature of this bot is currently under development!'
+                    fixtures_embed = discord.Embed(
+                        title='Fixtures',
+                        description='Fixtures for a week',
+                        color=discord.Color.dark_purple()
                     )
-                elif cmd == 'scores':
-                    await message.reply(
-                        'Sorry! \'scores\' feature of this bot is currently under development!'
+                    fixtures = get_fixtures() 
+                    if fixtures:
+                        for x in fixtures:
+                            fixtures_embed.add_field(
+                                name=x['name'],
+                                value=x['date']
+                            )
+                    await message.reply(embed=fixtures_embed)
+                elif cmd == 'results':
+                    results_embed = discord.Embed(
+                        title='Results',
+                        description='Results for recent matches',
+                        color=discord.Color.red()
                     )
+                    matches = get_recent_matches()
+                    if matches:
+                        for x in matches:
+                            results_embed.add_field(
+                                name=x['game'],
+                                value=x['utc_date']
+                            )
+                    await message.reply(embed=results_embed)
+                elif cmd.startswith('score'):
+                    if not ' ' in cmd:
+                        return
+                    _, match_id = cmd.split()
+                    try:
+                        score_embed = discord.Embed(
+                            title=f'Score for match: {match_id}',
+                            description=get_score(int(match_id)),
+                            color=discord.Color.red()
+                        )
+                        await message.reply(embed=score_embed)
+                    except:
+                        await message.reply('Sorry! error encountered!')
+
 
 client = BotClient()
 client.run(os.getenv('CLIENT_TOKEN'))
---
+++ provider.py
@@ -0,0 +1,60 @@
+''' Score provider for our bot
+
+This module returns the list of fixtures and recent cricket matches.
+'''
+
+import cricapi
+import dateutil.parser as dp
+from datetime import datetime
+
+import os
+
+from dotenv import load_dotenv
+load_dotenv()
+
+apikey = os.getenv('CRICAPI_KEY')
+cricapi = cricapi.Cricapi(apikey)
+
+def format_date(date):
+    ''' Format date for quick calculation '''
+    split_date = date.split()
+    split_date[1] = split_date[1][:3]
+    return datetime.strptime(' '.join(split_date), '%d %b %Y')
+
+def get_fixtures():
+    ''' Get the fixtures till a week from now '''
+    fixtures = []
+    for x in cricapi.matchCalendar()['data']:
+        if abs((format_date(x['date']) - datetime.utcnow()).days) <= 7:
+            fixtures.append({
+                'name': x['name'],
+                'date': x['date']
+            })
+    return fixtures
+
+def get_recent_matches():
+    ''' Get the recently concluded or ongoing matches '''
+    recent_matches = []
+    for x in cricapi.matches()['matches']:
+        if x['matchStarted'] == True:
+            current_utc_time = datetime.utcnow()
+            match_utc_time = dp.parse(x['dateTimeGMT']).replace(tzinfo=None)
+            if abs((current_utc_time - match_utc_time).days) <= 1.5:
+                formatted_data = {
+                    'id': x['unique_id'],
+                    'game': f'[{x["unique_id"]}] {x["team-1"]} vs {x["team-2"]}',
+                    'utc_date': match_utc_time.strftime('%m/%d/%Y, %-H:%M')
+                }
+                if 'toss_winner_team' in x.keys():
+                    formatted_data['game'] += f' (toss: {x["toss_winner_team"]})'
+                if 'winner_team' in x.keys():
+                    formatted_data['game'] += f' (winner: {x["winner_team"]})'
+                recent_matches.append(formatted_data)
+    return recent_matches
+
+def get_score(match_id):
+    ''' Get the score for a requested match '''
+    try:
+        return cricapi.cricketScore({'unique_id': match_id})['score']
+    except:
+        return 'Sorry! match not found or error detected!'
