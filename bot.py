78feb331429dfc5c94ba5d8af54a678db1c3ae5f
Shreyam Pokharel <pshreyam@gmail.com>
2021-02-24 17:59:50 +0545

    refactored code and used commands.Bot class

--- bot.py
+++ bot.py
@@ -1,90 +1,123 @@
 import os
 
 import discord
-
+from discord.ext import commands
+from discord.ext.commands import CommandNotFound
 from dotenv import load_dotenv
+
+from provider import get_fixtures, get_results, get_score
+
 load_dotenv()
 
-from provider import get_fixtures, get_recent_matches, get_score
+BOT_PREFIX = '%'
 
-class BotClient(discord.Client):
-    async def on_connect(self):
-        print('Connecting to discord!')
+bot = commands.Bot(
+    command_prefix=BOT_PREFIX, 
+    description='Helps with cricket scores!'
+)
+bot.remove_command('help')
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user} has connected to discord!')
+
+@bot.command(name='fixtures')
+async def send_fixtures(ctx, *args):
+    fixtures = discord.Embed(
+        title='Fixtures',
+        description='Fixtures for the next 7 days',
+        color=discord.Color.dark_purple()
+    )
+
+    fixtures_list = get_fixtures() 
     
-    async def on_ready(self):
-        print(f'Connected to discord as : {self.user}')
+    if fixtures_list:
+        for fixture in fixtures_list:
+            fixtures.add_field(
+                name=fixture['name'],
+                value=fixture['date']
+            )
+
+    await ctx.send(embed=fixtures)
+
+@bot.command(name='results')
+async def send_results(ctx, *args):
+    results = discord.Embed(
+        title='Results',
+        description='Results for recent matches',
+        color=discord.Color.red()
+    )
+
+    results_list = get_results()
     
-    async def on_message(self, message):
-        if message.author == self.user:
-            return 
-        else:
-            if message.content.startswith('?'):
-                cmd = str(message.content[1:])
-                if cmd == 'help':
-                    help_embed = discord.Embed(
-                        title='Help for CricHelperBot!',
-                        description='CricHelperBot is intended to help you with \
-                        cricket scores and fixtures!',
-                        color=discord.Color.blue()
-                    )
-                    help_embed.add_field(
-                        name='?fixtures', 
-                        value='get future cricket fixtures',
-                        inline=True
-                    )
-                    help_embed.add_field(
-                        name='?results', 
-                        value='get results for recent matches',
-                        inline=True
-                    )
-                    help_embed.add_field(
-                        name='?score <match_id>', 
-                        value='get score for the match with id',
-                        inline=True
-                    )
-                    await message.reply(embed=help_embed)
-                elif cmd == 'fixtures':
-                    fixtures_embed = discord.Embed(
-                        title='Fixtures',
-                        description='Fixtures for a week',
-                        color=discord.Color.dark_purple()
-                    )
-                    fixtures = get_fixtures() 
-                    if fixtures:
-                        for x in fixtures:
-                            fixtures_embed.add_field(
-                                name=x['name'],
-                                value=x['date']
-                            )
-                    await message.reply(embed=fixtures_embed)
-                elif cmd == 'results':
-                    results_embed = discord.Embed(
-                        title='Results',
-                        description='Results for recent matches',
-                        color=discord.Color.red()
-                    )
-                    matches = get_recent_matches()
-                    if matches:
-                        for x in matches:
-                            results_embed.add_field(
-                                name=x['game'],
-                                value=x['utc_date']
-                            )
-                    await message.reply(embed=results_embed)
-                elif cmd.startswith('score'):
-                    if not ' ' in cmd:
-                        return
-                    _, match_id = cmd.split()
-                    try:
-                        score_embed = discord.Embed(
-                            title=f'Score for match: {match_id}',
-                            description=get_score(int(match_id)),
-                            color=discord.Color.red()
-                        )
-                        await message.reply(embed=score_embed)
-                    except:
-                        await message.reply('Sorry! error encountered!')
+    if results_list:
+        for result in results_list:
+            results.add_field(
+                name=result['game'],
+                value=result['utc_date']
+            )
 
+    await ctx.send(embed=results)
 
-client = BotClient()
-client.run(os.getenv('CLIENT_TOKEN'))
+@bot.command(name='score')
+async def send_match_score(ctx, *args):
+    err_msg = 'Please provide a valid match id!'
+
+    if not args:
+        await ctx.send(err_msg)
+        return
+
+    try:
+        match_id = int(args[0])
+    except ValueError:
+        await ctx.send(err_msg)
+        return
+
+    score = discord.Embed(
+        title=f'Score for match: {match_id}',
+        description=get_score(match_id),
+        color=discord.Color.red()
+    )
+
+    await ctx.send(embed=score)
+
+@bot.command(name='help')
+async def send_help(ctx, *args):
+    help = discord.Embed(
+        title='Help for CricHelperBot!',
+        description=('CricHelperBot is intended to help you with '
+        'cricket scores and fixtures!'),
+        color=discord.Color.blue()
+    )
+
+    help.add_field(
+        name=f'{BOT_PREFIX}fixtures', 
+        value='get future cricket fixtures',
+        inline=True
+    )
+
+    help.add_field(
+        name=f'{BOT_PREFIX}results', 
+        value='get results for recent matches',
+        inline=True
+    )
+
+    help.add_field(
+        name=f'{BOT_PREFIX}score <match_id>', 
+        value='get score for the match with id',
+        inline=True
+    )
+
+    await ctx.send(embed=help)
+
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, CommandNotFound):
+        await ctx.send(('Sorry! No such command. '
+        f'Try running {BOT_PREFIX}help to check out '
+        'the list of available commands.'))
+        return
+
+    raise error
+
+bot.run(os.getenv('CLIENT_TOKEN'))
